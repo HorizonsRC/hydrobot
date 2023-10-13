e3e765a0480296d0f121743a8f7e8dbcb91a7c8d
Nicolas Mostert <Nicolas.Mostert@horizons.govt.nz>
2023-10-13 13:12:04 +1300

    Added prototype folder for ipython notebooks

--- hilltop_scripts.py
+++ hilltop_scripts.py
@@ -4,17 +4,23 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     base_url = "http://tsdata.horizons.govt.nz/"
-    hts = "Toha.hts"
-    site = "Manawatu at Teachers College"
-    measurement = "Flow"
-    from_date = "2012-01-22 10:50"
-    to_date = "2018-04-13 14:05"
+    hts = "boo.hts"
+    site = "Saddle Road"
+    measurement = "Groundwater"
+    from_date = "2021-01-01 00:00"
+    to_date = "2023-10-12 8:30"
     dtl_method = "trend"
 
     # Acquire the data
     data = data_acquisition.get_data(base_url, hts, site, measurement, from_date, to_date, dtl_method)
-    
-    # Perform spike removal using your 'remove_spikes' function
+
+    plt.figure(figsize=(10, 6))
+    plt.subplot(1, 1, 1)
+    plt.plot(data["Value"], label="Original Data")
+    plt.title("Data Before Spike Removal")
+    plt.legend()
+
+    # Perform spike removal using 'remove_spikes' function
     span = 10
     high_clip = 1000
     low_clip = -1000
@@ -22,16 +28,15 @@ if __name__ == "__main__":
     cleaned_data = smooth.remove_spikes(data["Value"], span, high_clip, low_clip, delta)
 
     # Plot the data before and after spike removal
-    plt.figure(figsize=(10, 6))
-    plt.subplot(2, 1, 1)
-    plt.plot(data["Value"], label="Original Data")
-    plt.title("Data Before Spike Removal")
-    plt.legend()
-
-    plt.subplot(2, 1, 2)
-    plt.plot(cleaned_data, label="Cleaned Data", color='orange')
-    plt.title("Data After Spike Removal")
-    plt.legend()
+    # plt.figure(figsize=(10, 6))
+    # plt.subplot(1, 1, 1)
+    # plt.plot(data["Value"], label="Original Data")
+    # plt.title("Data Before Spike Removal")
+    # plt.legend()
+    #
+    # plt.plot(cleaned_data, label="Cleaned Data", color='orange')
+    # plt.title("Data After Spike Removal")
+    # plt.legend()
 
     plt.tight_layout()
     plt.show()
