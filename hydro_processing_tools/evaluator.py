6531082bd8f12b900238518982de5673a3ae5731
Sirvine <sam.irvine@horizons.govt.nz>
2023-10-31 14:44:33 +1300

    More gap finder and starting gap_finder tests

--- hydro_processing_tools/evaluator.py
+++ hydro_processing_tools/evaluator.py
@@ -5,7 +5,9 @@ import numpy as np
 
 def gap_finder(data):
     """
-    Finds gaps in a series of data (defined by isnull())
+    Finds gaps in a series of data (indicated by np.isnan())
+
+    Returns a list of tuples indicating the start of the gap, and the number of entries that are NaN
 
     Parameters:
     -----------
@@ -25,4 +27,52 @@ def gap_finder(data):
     out_idx = idx[valid_mask]
     out_count = count[valid_mask]
     out = zip(data.index[out_idx], out_count)
-    return [o for o in out]
+
+    return list(out)
+
+
+def small_gap_closer(series, gap_length):
+    """
+    Removes small gaps from a series
+
+    Gaps are defined by a sequential number of np.NaN values
+    Small gaps are defined as gaps of length gap_length or less
+
+    Will return series with the nan values in the short gaps removed, and the long gaps untouched
+
+    :param series: pandas.Series
+        Data which has gaps to be closed
+    :param gap_length: integer
+        Maximum length of gaps removed, will remove all np.NaN's in consecutive runs of gap_length or less
+    :return:
+    pandas.Series
+        Data with any short gaps removed
+    """
+
+    gaps = gap_finder(series)
+    for gap in gaps:
+        if gap[1] <= gap_length:
+            mask = ~series.index.isin(
+                series.index[
+                    series.index.get_loc(gap[0]) : series.index.get_loc(gap[0]) + gap[1]
+                ]
+            )
+            series = series[mask]
+
+    return series
+
+
+def check_data_quality_code(series, check_series, measurement):
+    """
+    Quality codes data based on the difference between the standard series and the check data
+
+    :param series: pd.Series
+        Data to be quality coded
+    :param check_series: pd.Series
+        Check data
+    :param measurement: data_sources.Measurement
+        Handler for QC comparisons
+
+    :return:
+    """
+    return []
--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -18,15 +18,15 @@ gap_data_dict = {
     "2021-01-01 00:00": np.NaN,
     "2021-01-01 00:15": 2.0,
     "2021-01-01 00:30": np.NaN,
-    "2021-01-01 00:45": pd.NA,
+    "2021-01-01 00:45": np.NaN,
     "2021-01-01 01:00": 5.0,
-    "2021-01-01 01:15": pd.NA,
-    "2021-01-01 01:30": pd.NA,
+    "2021-01-01 01:15": np.NaN,
+    "2021-01-01 01:30": np.NaN,
     "2021-01-01 01:45": np.NaN,
-    "2021-01-01 01:00": 0.0,
-    "2021-01-01 01:15": 0.0,
-    "2021-01-01 01:30": pd.NA,
-    "2021-01-01 01:45": pd.NA,
+    "2021-01-01 02:00": 0.0,
+    "2021-01-01 02:15": 0.0,
+    "2021-01-01 02:30": np.NaN,
+    "2021-01-01 02:45": np.NaN,
 }
 
 
@@ -46,6 +46,7 @@ def gap_data():
     return data_series
 
 
+@pytest.mark.dependency(name="test_gap_finder")
 def test_gap_finder(raw_data, gap_data):
     no_gap_list = evaluator.gap_finder(raw_data)
     assert no_gap_list == [], "Gap found where there should be no gap"
@@ -56,3 +57,33 @@ def test_gap_finder(raw_data, gap_data):
     assert gap_list[0][1] == 1, "gap length of 1 not calculated correctly"
     assert gap_list[1][1] == 2, "gap length of 2 not calculated correctly"
     assert gap_list[2][1] == 3, "gap length of 3 not calculated correctly"
+
+
+@pytest.mark.dependency(name="test_small_gap_closer")
+def test_small_gap_closer(raw_data, gap_data):
+    # No gaps here, nothing should happen
+    no_gaps = evaluator.small_gap_closer(raw_data, 1)
+    assert no_gaps.equals(raw_data), "Data without gaps should not be modified, but was"
+
+    # All gaps should be closed
+    removed_gaps = evaluator.small_gap_closer(gap_data, 5)
+    assert len(removed_gaps) == 4, "Data changed during gap closing"
+
+    # Should still have one gap of len 3, others closed
+    some_gaps = evaluator.small_gap_closer(gap_data, 2)
+    assert len(some_gaps) == 7, "gap_finder did not find one or more of the gaps"
+
+
+@pytest.mark.dependency(depends=["test_gap_finder", "test_small_gap_closer"])
+def test_small_gap_closer_part2(raw_data, gap_data):
+    # All gaps should be closed
+    removed_gaps = evaluator.small_gap_closer(gap_data, 5)
+    assert evaluator.gap_finder(removed_gaps) == [], "Gap not closed!"
+
+    # Should still have one gap of len 3, others closed
+    some_gaps = evaluator.small_gap_closer(gap_data, 2)
+    assert len(evaluator.gap_finder(some_gaps)) < 2, "Not enough gaps were removed"
+    assert len(evaluator.gap_finder(some_gaps)) > 0, "Too many gaps were removed"
+    assert (
+        evaluator.gap_finder(some_gaps)[0][1] == 3
+    ), "incorrect gap length after gap closure"
