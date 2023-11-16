a9af2d63fe97b0628f6e442b1f9c020aad9576a4
Sirvine <sam.irvine@horizons.govt.nz>
2023-11-16 17:03:54 +1300

    STAGE DATA BOI

--- hydro_processing_tools/data_sources.py
+++ hydro_processing_tools/data_sources.py
@@ -19,9 +19,7 @@ class Measurement:
         self.name = name
 
     def __repr__(self):
-        return repr(
-            f"Measurement {self.name} with limits ({self.qc_500_limit},{self.qc_600_limit})"
-        )
+        return repr(f"Measurement '{self.name}' with limits {vars(self)}")
 
     @annalizer.annalize
     def find_qc(self, base_datum, check_datum):
@@ -35,6 +33,53 @@ class Measurement:
             return 400
 
 
+class TwoLevelMeasurement(Measurement):
+    """
+    Measurement for standards such as water level
+    Fixed error up to given threshold, percentage error after that
+    """
+
+    @annalizer.annalize
+    def __init__(
+        self,
+        qc_500_limit,
+        qc_600_limit,
+        qc_500_percent,
+        qc_600_percent,
+        limit_percent_threshold,
+        name="",
+    ):
+        Measurement.__init__(self, qc_500_limit, qc_600_limit)
+        # self.qc_500_limit = qc_500_limit
+        # self.qc_600_limit = qc_600_limit
+        self.qc_500_percent = qc_500_percent
+        self.qc_600_percent = qc_600_percent
+        self.limit_percent_threshold = limit_percent_threshold
+        self.name = name
+
+    @annalizer.annalize
+    def find_qc(self, base_datum, check_datum):
+        """Find the base quality codes with two a flat and percentage QC threshold"""
+        if base_datum < self.limit_percent_threshold:
+            # flat qc check
+            diff = np.abs(base_datum - check_datum)
+            if diff < self.qc_600_limit:
+                return 600
+            elif diff < self.qc_500_limit:
+                return 500
+            else:
+                return 400
+        else:
+            # percent qc check
+            diff = np.abs(base_datum / check_datum - 1) * 100
+            if diff < self.qc_600_percent:
+                return 600
+            elif diff < self.qc_500_percent:
+                return 500
+            else:
+                return 400
+
+
 @annalizer.annalize
 def get_measurement_dict():
     """
@@ -43,12 +88,30 @@ def get_measurement_dict():
     """
     measurement_dict = {}
     script_dir = Path(__file__).parent
+
+    # Plain Measurements
     template_path = (script_dir / "config/measurement_QC_config.csv").resolve()
     with open(template_path) as csv_file:
         reader = csv.reader(csv_file)
 
         for row in reader:
             measurement_dict[row[0]] = Measurement(float(row[1]), float(row[2]), row[0])
+        csv_file.close()
+
+    # Two stage Measurements
+    template_path = (script_dir / "config/TwoLevelMeasurement_QC_config.csv").resolve()
+    with open(template_path) as csv_file:
+        reader = csv.reader(csv_file)
+
+        for row in reader:
+            measurement_dict[row[0]] = TwoLevelMeasurement(
+                float(row[1]),
+                float(row[2]),
+                float(row[3]),
+                float(row[4]),
+                float(row[5]),
+                row[0],
+            )
         csv_file.close()
 
     return measurement_dict
