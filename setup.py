b40114e1b0ade3b54f8d1d9bbee9b6ef35f92188
Sirvine <sam.irvine@horizons.govt.nz>
2023-12-12 09:50:56 +1300

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/nicmostert/hydrobot",
-    version="0.3.0",
+    version="0.3.1",
     zip_safe=False,
     package_data={"hydrobot": ["config/*"]},
 )
