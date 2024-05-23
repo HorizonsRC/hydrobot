64e9f062bf62a61854e68ca11557a1f839bfc799
Sirvine1994 <132408772+Sirvine1994@users.noreply.github.com>
2024-05-23 14:35:05 +1200

    Triad class start

---
+++ hydrobot/triad.py
@@ -0,0 +1,8 @@
+"""Triad class."""
+
+
+class Triad:
+    """A container for Time series data - up to standard, check, quality."""
+
+    def __init__(self, standard_series=None):
+        """Constructs the triad."""
