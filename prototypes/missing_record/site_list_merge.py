570d2299e5be1f29f5f4fba0ec6952e3700a7d8f
nicmostert <nicolas.mostert@horizons.govt.nz>
2024-08-15 16:36:47 +1200

    Pushing because I'm done for the day

--- prototypes/missing_record/site_list_merge.py
+++ prototypes/missing_record/site_list_merge.py
@@ -23,22 +23,22 @@ engine = db.create_engine(connection_url)
 
 sites_query = """
 SELECT Sites.SiteID, SiteName
-FROM Sites
-INNER JOIN RegionSites on Sites.SiteID = RegionSites.SiteID
-INNER JOIN Regions on RegionSites.RegionID = Regions.RegionID
-WHERE
-    (
-        Regions.RegionName = 'CENTRAL'
-        OR Regions.RegionName = 'EASTERN'
-        OR Regions.RegionName = 'NORTHERN'
-        OR Regions.RegionName = 'LAKES AND WQ'
-    )
-    AND
-    (
-        RecordingAuthority1 = 'MWRC'
-        OR RecordingAuthority2 = 'MWRC'
-    )
-    AND Inactive = 0
+    FROM Sites
+    INNER JOIN RegionSites on Sites.SiteID = RegionSites.SiteID
+    INNER JOIN Regions on RegionSites.RegionID = Regions.RegionID
+    WHERE
+        (
+            Regions.RegionName = 'CENTRAL'
+            OR Regions.RegionName = 'EASTERN'
+            OR Regions.RegionName = 'NORTHERN'
+            OR Regions.RegionName = 'LAKES AND WQ'
+        )
+        AND
+        (
+            RecordingAuthority1 = 'MWRC'
+            OR RecordingAuthority2 = 'MWRC'
+        )
+        AND Inactive = 0
 """
 
 # execute the sql_query
