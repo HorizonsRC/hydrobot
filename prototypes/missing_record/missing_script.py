1c2c6941a14065d446910be780684d48b718d0ad
Sirvine1994 <132408772+Sirvine1994@users.noreply.github.com>
2024-08-16 16:39:36 +1200

    Now taking from functions

--- prototypes/missing_record/missing_script.py
+++ prototypes/missing_record/missing_script.py
@@ -1,13 +1,18 @@
 """Missing record script."""
 import csv
 import time
+import warnings
 
 import numpy as np
 import pandas as pd
+import site_list_merge
 import yaml
+from pandas.tseries.frequencies import to_offset
 
 from hydrobot.data_acquisition import get_data
 from hydrobot.utils import infer_frequency
+
+warnings.filterwarnings("ignore", message=".*Empty hilltop response:.*")
 
 with open("script_config.yaml") as file:
     config = yaml.safe_load(file)
@@ -33,20 +38,42 @@ def report_missing_record(site, measurement, start, end):
     freq = infer_frequency(series.index, method="mode")
     series = series.reindex(pd.date_range(start, end, freq=freq))
     missing_points = series.asfreq(freq).isna().sum()
-    return str(missing_points * pd.to_timedelta(freq))
+    return str(missing_points * pd.to_timedelta(to_offset(freq)))
 
+
+sites = site_list_merge.get_sites().head()
+# measurements_df = site_list_merge.get_measurements()
+# measurements = measurements_df.MeasurementFullName
+
+with open("Active_Measurements.csv", newline="") as f:
+    reader = csv.reader(f)
+    measurements = [row[0] for row in reader]
 
 a = {}
 start_timer = time.time()
-for site in config["sites"]:
+for site in sites.SiteName:
     b = []
-    for meas in config["measurements"]:
-        b.append(report_missing_record(site, meas, config["start"], config["end"]))
+    for meas in measurements:
+        try:
+            b.append(report_missing_record(site, meas, config["start"], config["end"]))
+        except ValueError as e:
+            print(f"Site '{site}' with meas '{meas}' doesn't work: {e}")
+            b.append(np.nan)
     a[site] = b
     print(site, time.time() - start_timer)
 
 with open("output_dump/output.csv", "w", newline="") as output:
     wr = csv.writer(output)
-    wr.writerow(["Sites"] + config["measurements"])
+    wr.writerow(["Sites"] + measurements)
     for site in a:
         wr.writerow([site] + a[site])
+
+diff = pd.to_datetime(config["end"]) - pd.to_datetime(config["start"])
+
+with open("output_dump/output_percent.csv", "w", newline="") as output:
+    wr = csv.writer(output)
+    wr.writerow(["Sites"] + measurements)
+    for site in a:
+        wr.writerow(
+            [site] + [100 * i / diff if i is not np.NaN else np.NaN for i in a[site]]
+        )
--- prototypes/missing_record/site_list_merge.py
+++ prototypes/missing_record/site_list_merge.py
@@ -21,44 +21,58 @@ connection_url = URL.create(
 )
 engine = db.create_engine(connection_url)
 
-sites_query = """
-SELECT Sites.SiteID, SiteName, Regions.RegionName
-    FROM Sites
-    INNER JOIN RegionSites on Sites.SiteID = RegionSites.SiteID
-    INNER JOIN Regions on RegionSites.RegionID = Regions.RegionID
-    WHERE
-        (
-            Regions.RegionName = 'CENTRAL'
-            OR Regions.RegionName = 'EASTERN'
-            OR Regions.RegionName = 'NORTHERN'
-            OR Regions.RegionName = 'LAKES AND WQ'
-        )
-        AND
-        (
-            RecordingAuthority1 = 'MWRC'
-            OR RecordingAuthority2 = 'MWRC'
-        )
-        AND Inactive = 0
-"""
 
-# execute the sql_query
-site_list = pd.read_sql(sites_query, engine)
+def get_sites():
+    """
+    Gives sites to check for missing data.
 
-print(site_list.head())
+    Returns
+    -------
+    pd.Dataframe
+        All relevant sites + id + region
+    """
+    sites_query = """
+        SELECT Sites.SiteID, SiteName, Regions.RegionName
+            FROM Sites
+            INNER JOIN RegionSites on Sites.SiteID = RegionSites.SiteID
+            INNER JOIN Regions on RegionSites.RegionID = Regions.RegionID
+            WHERE
+                (
+                    Regions.RegionName = 'CENTRAL'
+                    OR Regions.RegionName = 'EASTERN'
+                    OR Regions.RegionName = 'NORTHERN'
+                    OR Regions.RegionName = 'LAKES AND WQ'
+                )
+                AND
+                (
+                    RecordingAuthority1 = 'MWRC'
+                    OR RecordingAuthority2 = 'MWRC'
+                )
+                AND Inactive = 0
+        """
+    # execute the sql_query
+    return pd.read_sql(sites_query, engine)
 
-measurements_query = """
-SELECT MeasurementID, MeasurementName, DataSourceName
-    FROM Measurements
-    WHERE IsCheckDataOnly = 0
-"""
 
-measurement_list = pd.read_sql(measurements_query, engine)
+def get_measurements():
+    """
+    Gives measurements to check for missing data.
 
-measurement_list["MeasurementFullName"] = (
-    measurement_list["MeasurementName"]
-    + " ["
-    + measurement_list["DataSourceName"]
-    + "]"
-)
-
-print(measurement_list.head())
+    Returns
+    -------
+    pd.Dataframe
+        All measurements with data sources
+    """
+    measurements_query = """
+        SELECT MeasurementID, MeasurementName, DataSourceName
+            FROM Measurements
+            WHERE IsCheckDataOnly = 0
+        """
+    measurement_list = pd.read_sql(measurements_query, engine)
+    measurement_list["MeasurementFullName"] = (
+        measurement_list["MeasurementName"]
+        + " ["
+        + measurement_list["DataSourceName"]
+        + "]"
+    )
+    return measurement_list
